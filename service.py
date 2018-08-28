ee7c48c3192c91013887394f7c7ef4709c5f8dbd
JMFedorov <jmfedorov@gmail.com>
2018-08-28 11:04:49 +0300

    default answer remove

--- service.py
+++ service.py
@@ -269,6 +269,8 @@ def message_processing(uid, text):
             mt.send_message(uid, cnst.MSG_ADMIN_PANEL, cnst.KEYBOARD_ADMIN)
         else:
             mt.send_message(uid, cnst.MSG_YOU_NOT_ADMIN, cnst.KEYBOARD_USER)
+    elif text.lower() == "clearME":
+        mt.send_message(uid, "clear", keyboard=cnst.EMPTY_KEYBOARD)
     else:
         thread = mt.ThreadNewUserOrNote(uid, uname)
         thread.start()
